57bc9b0e72971e083013f454823c954d629e0c91
karl2365 <karlbeksa@gmail.com>
2020-03-27 12:45:07 -0400

    WIP errors in ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,15 +8,44 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # check if length of storage is zero
+        if len(self.storage) == 0:
+            # if is set new item to head
+            self.storage.add_to_head(item)
+            # and set current to head
+            self.current = self.storage.head
+        # elif length of strorage list is less than capacity
+        elif len(self.storage) < self.capacity:
+            # add new item to the tail of list
+            self.storage.add_to_tail(item)
+        # elif storage list is at capacity
+        elif len(self.storage) == self.capacity:
+            # Check if self.current.next is None
+            if self.current.next:
+                # replace value of current with new item
+                self.current.value = item
+                # increment the current item to next node
+                self.current = self.current.next
+            else:
+                 # replace value of current with new item
+                self.current.value = item
+                # increment the current item back to beginning of list
+                self.current = self.storage.head
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
-
+        # start at beginning of list
+        node = self.storage.head
+        while(node):
+            list_buffer_contents.append(node.value)
+            node = node.next
         return list_buffer_contents
+
+
+
 
 # ----------------Stretch Goal-------------------
 
