4e1ad0187db1506e4170ebb47ed2f45dce1972fa
karl2365 <karlbeksa@gmail.com>
2020-03-27 13:26:39 -0400

    MVP complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,7 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.current = None
-        
+
         self.storage = DoublyLinkedList()
 
     def append(self, item):
